49306e3327a11c44f77c5424ec68abcf4890f9fd
dragosbus <dragosbusuioc94@gmail.com>
2018-05-15 16:51:39 +0200

    created the form with 2 inputs and one select element

--- module3/assigment/app.jsx
+++ module3/assigment/app.jsx
@@ -1,7 +1,10 @@
 class App extends React.Component {
     render() {
         return (
-            <Header/>
+            <div>
+                <Header />
+                <Form />
+            </div>
         ); 
     }
 }
@@ -15,7 +18,33 @@ const Header = () => {
 };
 
 class Form extends React.Component {
-    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <form id="register-form">
+                <div>
+                    <label htmlFor="first-name">First Name</label>
+                    <input type="text" id="first-name"/>
+                </div>
+                <div>
+                    <label htmlFor="last-name">First Name</label>
+                    <input type="text" id="last-name" />
+                </div>
+                <div>
+                    <label htmlFor="select-activity">First Name</label>
+                    <select value={this.props.activity} id="select-activity">
+                        <option value="Science Lab">Science Lab</option>
+                        <option value="Swimming">Swimming</option>
+                        <option value="Cooking">Cooking</option>
+                        <option value="Painting">Painting</option>
+                    </select>
+                </div>
+            </form>
+        );
+    }
 }
 
 
