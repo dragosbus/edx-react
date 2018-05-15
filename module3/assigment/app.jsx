99a57792db50d7028cb2feeefcb0df886785de65
dragosbus <dragosbusuioc94@gmail.com>
2018-05-15 17:10:46 +0200

    finished adding all elements fot the form and style

--- module3/assigment/app.jsx
+++ module3/assigment/app.jsx
@@ -25,16 +25,16 @@ class Form extends React.Component {
     render() {
         return (
             <form id="register-form">
-                <div>
+                <div className="first-name">
                     <label htmlFor="first-name">First Name</label>
                     <input type="text" id="first-name"/>
                 </div>
-                <div>
+                <div className="last-name">
                     <label htmlFor="last-name">First Name</label>
                     <input type="text" id="last-name" />
                 </div>
-                <div>
-                    <label htmlFor="select-activity">First Name</label>
+                <div className="select">
+                    <label htmlFor="select-activity">Select Activity</label>
                     <select value={this.props.activity} id="select-activity">
                         <option value="Science Lab">Science Lab</option>
                         <option value="Swimming">Swimming</option>
@@ -42,6 +42,22 @@ class Form extends React.Component {
                         <option value="Painting">Painting</option>
                     </select>
                 </div>
+                <div className="checks">
+                    <label>Check All that apply</label>
+                    <div className="check">
+                        <input type="checkbox" id="a"/>
+                        <label htmlFor="a">a) Dietary Restrictions</label>
+                    </div>
+                    <div className="check">
+                        <input type="checkbox" id="b" />
+                        <label htmlFor="b">b) Physical Disabilities</label>
+                    </div>
+                    <div className="check">
+                        <input type="checkbox" id="c" />
+                        <label htmlFor="c">c) Medical Needs</label>
+                    </div>
+                </div>
+                <button type="submit">Register</button>
             </form>
         );
     }
